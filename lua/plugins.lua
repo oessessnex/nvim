986c9304c8f530e4c712dc1e5614ac54df0ecfc9
oessessnex <oessessnex@protonmail.com>
2023-06-11 18:56:48 +0200

    Try adding packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,7 @@
 local plugins_path = vim.fn.stdpath('config') .. "/pack"
 
 require 'packer'.startup({function(use)
-  use "norcalli/nvim-colorizer.lua"
+
 end, config = {
   package_root = plugins_path,
   plugin_package = "plugins"
